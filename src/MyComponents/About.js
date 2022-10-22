70e53832063c326be61c4433469a6046337a2bdc
Vivek Kumar GG <vivek.gg.kumar@ericsson.com>
2022-10-22 17:37:00 +0530

    Initial commit for to-do app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    let minHight = {
+        minHeight: '79vh'
+    }
+    return (
+        <div className='container' style={minHight}>
+            <h5 className='text-center my-3'>This is my practice website</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+        </div>
+    )
+}
